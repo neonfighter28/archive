30d053f8a43ea47418e6d8401d4d5a0d7e118cf7
neonfighter28 <58990354+neonfighter28@users.noreply.github.com>
2023-05-31 02:01:55 +0200

    script enhancements

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 import requests
+import sys
+import html
+
+if sys.argv[1]:
+    filename = sys.argv[1]
+else:
+    print("No filename given")
+    sys.exit()
 
 URL = (
     "https://translate.googleapis.com/language/translate/v2"
@@ -10,7 +18,7 @@ PARAMS = {
     "target": "en"
 }
 
-with open("input.txt", "r") as file:
+with open(filename, "r") as file:
     text = file.read()
 
 DATA = {
@@ -19,6 +27,6 @@ DATA = {
 
 response = requests.post(URL, params = PARAMS, json=DATA)
 translated = response.json()["data"]["translations"][0]["translatedText"]
-
-with open("translated.txt", "w+", encoding="utf-8") as f:
-    f.write(translated)
+decoded_text = html.unescape(translated)
+with open(f"{filename.split('.')[0]}-EN.txt", "w+", encoding="utf-8") as f:
+    f.write(decoded_text)
--- ocr.sh
+++ ocr.sh
@@ -1,17 +1,49 @@
 #!/bin/bash
 
+# Check if argument is supplied and set filename
+if [ $# -eq 0 ]
+  then
+    echo "Supply a PDF file as argument"
+    exit 1
+fi
+r_filename=$1
+filename="${r_filename%.*}"
+
+# Check if python environment exists
+if [ ! -d "./venv" ]
+  then
+    echo "Python environment not found. Creating..."
+    python3 -m venv venv
+    source ./venv/bin/activate
+    pip3 install -r requirements.txt
+fi
 
 # Source the python environment
 source ./venv/bin/activate
 
+# Check if tesseract is installed
+if ! command -v tesseract &> /dev/null
+then
+    echo "tesseract could not be found. Installing..."
+    sudo apt install tesseract-ocr
+    sudo apt install tesseract-ocr-deu
+fi
+
+# Check if pdftoppm is installed
+if ! command -v pdftoppm &> /dev/null
+then
+    echo "pdftoppm could not be found. Installing..."
+    sudo apt install poppler-utils
+fi
+
 # Make temporary folder and copy file into it
 mkdir .temp
-cp input.pdf .temp/input.pdf
+cp $1 .temp/$1
 cp main.py .temp/main.py
 cd .temp
 
 echo "Splitting PDF to PNGs"
-pdftoppm -png input.pdf TEMP_PNG
+pdftoppm -png $1 TEMP_PNG
 
 echo "Done"
 for i in TEMP_PNG-??.png; 
@@ -20,16 +52,22 @@ do
     tesseract "$i" "TEMP_TEXT-$i" -l deu; 
 
 done;
+
 echo "Concatenating text files"
-cat TEMP_TEXT* > input.txt
+
+
+cat *.txt > $filename.txt
 
 echo "Removing temporary files..."
 rm TEMP*
 
 echo "Translating"
-python main.py
+python3 main.py $filename.txt
 
 cd ..
-cp .temp/translated.txt .
-cp .temp/input.txt .
+
+cp .temp/$filename-EN.txt .
+cp .temp/$filename.txt .
 rm -r .temp
+
+echo "Done"
